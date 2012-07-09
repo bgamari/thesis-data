13c6ea39b516f650d064248f0f53c230bca61cfe
Ben Gamari <bgamari.foss@gmail.com>
2012-07-09 14:27:40 -0400

    Update mirror-peker

--- peker/mirror-peker.sh
+++ peker/mirror-peker.sh
@@ -2,4 +2,7 @@
 
 rsync -a --progress ~lab/data/peker/Droplet_Stuff/ droplet-stuff
 git add `find -iname README`
+git commit -m "peker: Add READMEs"
+git annex add .
+git commit -m "peker: Add other files"
 
