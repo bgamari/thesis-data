2aa0fb60fc67484e7aec0c176fc9476addad02a2
Ben Gamari <bgamari.foss@gmail.com>
2012-11-17 12:23:54 -0500

    Compute confidence intervals

--- pan_li/force-extension/correlation.py
+++ pan_li/force-extension/correlation.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python
 
 import numpy as np
-from numpy import exp, log, log10
+from numpy import exp, log, log10, newaxis, abs
 import scipy.signal
 import matplotlib.pyplot as pl
 from mpl_toolkits.axes_grid1 import Grid
 from scipy.fftpack import fft, ifft
 from scipy.interpolate import interp1d
 from scipy.optimize import curve_fit
+from statsmodels.tsa.stattools import acf
 
 # left, bottom, width, height
 rect = (0.18, 0.20, 0.75, 0.73)
@@ -25,9 +26,11 @@ def autocorr(d, dt=6e-3):
     return taus, corr/norm
 
 def correlation_plot(d, dt=6e-3, **kwargs):
-    taus, corr = autocorr(d, dt)
+    corr, conf = acf(d, nlags=len(d)-1, alpha=0.05)
+    taus = dt*np.arange(0, len(d))
     ax = pl.gca()
-    ax.semilogx(taus, corr, **kwargs)
+    ax.plot(taus, corr, **kwargs)
+    ax.fill_between(taus, y1=conf[:,0], y2=conf[:,1], color='k', alpha=0.2, lw=0)
     ax.set_xscale('log')
     ax.set_xlabel(r'$\tau$ (seconds)')
     ax.set_ylabel(r'$G(\tau)$')
@@ -44,12 +47,12 @@ def plot_force_extension(d):
     grid[1].set_ylabel('force')
     grid[1].set_xlabel('time (s)')
 
-pl.clf()
-pl.hist(d['time'][1:] - d['time'][:-1], range=(-1e-3, 1e-2))
-pl.xlabel('inter-sample time (s)')
-pl.ylabel('counts')
-pl.savefig('jitter.png')
-pl.clf()
+#pl.clf()
+#pl.hist(d['time'][1:] - d['time'][:-1], range=(-1e-3, 1e-2))
+#pl.xlabel('inter-sample time (s)')
+#pl.ylabel('counts')
+#pl.savefig('jitter.png')
+#pl.clf()
 
 def smooth(x, window):
     s = np.r_[x, x[-1:-window:-1]]
@@ -70,7 +73,7 @@ dt = 5e-3
 xs = np.arange(min(d['time']), max(d['time']), dt)
 interp = interp1d(d['time'], d['ext'], kind='linear')(xs)
 pl.figure()
-correlation_plot(interp, dt=dt, c='b', marker='+', label='ext')
+correlation_plot(interp, dt=dt, c='darkblue', marker=',', linestyle=' ', label='ext')
 
 def ornstein_uhlenbeck(tau, taud, offset=0, alpha=1, amp=1):
     return offset + amp * exp(-(tau/taud)**alpha)
@@ -80,10 +83,10 @@ fit_taus = taus[taus<1e1]
 fit_corr = corr[taus<1e1]
 (params, cov) = curve_fit(ornstein_uhlenbeck,
                           fit_taus, fit_corr,
-                          p0=[1e-3, 0, 1])
+                          p0=[1e-3, 0, 1, 1])
 print 'Chi^2', np.sum((ornstein_uhlenbeck(fit_taus, *params) - fit_corr)**2)
 
-(taud,offset,alpha) = params
+(taud,offset,alpha,amp) = params
 print params
 taus_ = np.logspace(-3, log10(max(taus)), 1000)
 pl.semilogx(taus_, ornstein_uhlenbeck(taus_, *params), '-k', label='Model')
