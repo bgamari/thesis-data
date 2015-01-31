f6761e62b4b4560308aa1cc56493854c479f9212
Ben Gamari <ben@smart-cactus.org>
2015-01-30 23:15:47 -0500

    Add README and run script

---
+++ by-date/2015/2015-01/2015-01-30/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+irf=009
+
+cit_corrs="001 002 003 004 005 006 007 008"
+pipes_corrs="010 011 012 013 014 015 016 017"
+corrs="$cit_corrs $pipes_corrs"
+
+files="--irf=run${irf}.pt3.ch1.txt --irf=run${irf}.pt3.ch2.txt"
+for c in $corrs; do
+        files="$files run${c}.pt3.ch1.txt run${c}.pt3.ch2.txt"
+done
+
+python ~/lori/analysis/photon-tools/anisotropy $files -p2 -c2 -o fits.png >| fits.txt
