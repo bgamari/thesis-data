9d96fab8fb1ebca5abe4d356b96b7566ed5d727e
Ben Gamari <ben@smart-cactus.org>
2015-01-31 10:50:00 -0500

    2015-01-19: Add run script

---
+++ by-date/2015/2015-01/2015-01-19/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+irf=000
+
+corrs="001 002 003 004 005 006"
+
+files="--irf=run${irf}.pt3.ch1.txt --irf=run${irf}.pt3.ch2.txt"
+for c in $corrs; do
+        files="$files run${c}.pt3.ch1.txt run${c}.pt3.ch2.txt"
+done
+
+python ~/lori/analysis/photon-tools/anisotropy $files -p2 -c2 -o fits.png >| fits.txt
