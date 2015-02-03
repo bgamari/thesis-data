570d0c8cf1b92c62e1a5ab7786491332d12ad284
Ben Gamari <ben@smart-cactus.org>
2015-02-03 15:32:07 -0500

    git-annex automatic sync

--- by-date/2015/2015-01/2015-01-31/run.sh
+++ by-date/2015/2015-01/2015-01-31/run.sh
@@ -3,7 +3,7 @@
 irf=016
 
 cit_corrs="009 010 011 012 013 014 015"
-pipes_corrs="001 002 003 004 005 006 007 008"
+#pipes_corrs="001 002 003 004 005 006 007 008"
 corrs="$cit_corrs $pipes_corrs"
 
 files="--irf=run${irf}.pt3.ch1.txt --irf=run${irf}.pt3.ch2.txt"
@@ -11,4 +11,4 @@ for c in $corrs; do
         files="$files run${c}.pt3.ch1.txt run${c}.pt3.ch2.txt"
 done
 
-python ~/lori/analysis/photon-tools/anisotropy $files -p2 -c2 -o fits.png >| fits.txt
+python ~/lori/analysis/photon-tools/anisotropy $files -p2 -c2 -o fits.png -e >| fits.txt
