2041f6f255d47f64e970e829024b1c7271bcc98c
Ben Gamari <ben@smart-cactus.org>
2015-02-03 19:01:29 -0500

    git-annex automatic sync

---
+++ by-date/2015/2015-02/2015-02-03/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+irf=000
+
+corrs="001 002 003 004 005" # Oregon Green 488
+corrs="006 007 008 009" # BCECF
+
+files="--irf=run${irf}.pt3.ch1.txt --irf=run${irf}.pt3.ch2.txt"
+for c in $corrs; do
+        files="$files run${c}.pt3.ch1.txt run${c}.pt3.ch2.txt"
+done
+
+python ~/lori/analysis/photon-tools/anisotropy $files -p2 -c2 -o fits.png >| fits.txt
+
