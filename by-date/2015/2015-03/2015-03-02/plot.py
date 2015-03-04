49de29c8d3abb3eff971b506c856726043b39d90
Ben Gamari <ben@smart-cactus.org>
2015-03-04 09:08:18 -0500

    git-annex automatic sync

--- by-date/2015/2015-03/2015-03-02/plot.py
+++ by-date/2015/2015-03/2015-03-02/plot.py
@@ -3,13 +3,34 @@
 import matplotlib.pyplot as pl
 import numpy as np
 
-tau1 = 2307
-tau2 = 4718
+tau1 = 2250
+tau2 = 4623
 
-a = np.genfromtxt('results.txt', dtype=None, names=True)
-#pl.plot(a['actual_pH'], a['fraction'], 'o')
-f = a['fraction']
-pl.plot(a['actual_pH'], f * tau1 / (f * tau1 + (100-f) * tau2), 'o')
+phs = {
+    'cit2.5': 2.57,
+    'cit3.0': 3.15,
+    'cit3.5': 3.57,
+    'cit4.0': 4.09,
+    'cit4.5': 4.56,
+    'cit5.0': 5.09,
+    'cit5.5': 5.75,
+    
+    'phos6.0': 5.95,
+    'phos6.5': 6.37,
+    'phos7.0': 6.87,
+    'phos7.5': 7.39,
+    'phos8.0': 7.33,
+}
+
+results = np.genfromtxt('results.txt', dtype=None, names=True)
+ph = np.array([phs.get(res['buffer'], 0) for res in results])
+
+for buff in ['cit', 'phos']:
+    b = np.char.find(results['buffer'], buff) != -1
+    f = results['fraction'][b]
+    pl.plot(ph[b], f * tau1 / (f * tau1 + (100-f) * tau2), 'o')
+
+#pl.plot(ph, results['fraction'], 'o')
 #pl.ylim(0,100)
 pl.show()
 
--- by-date/2015/2015-03/2015-03-02/run.sh
+++ by-date/2015/2015-03/2015-03-02/run.sh
@@ -2,15 +2,23 @@
 
 irf=004
 
-# run005 is funny
-corrs="           006 007 008 009 010 011" # Citrate
-corrs="$corrs 012 013 014 015 016"         # Phosphate
-corrs="$corrs 017 018 019 020 021"         # Phosphate
-corrs="$corrs 022 023 024 025 026 027 028" # Citrate
+# Citrate: run005 is funny
+cit_corrs="006 007 008 009 010 011"
+cir_corrs="$cit_corrs 022 023 024 025 026 027 028"
+# Phosphate
+phos_corrs="012 013 014 015 016"
+phos_corrs="$phos_corrs 017 018 019 020 021"
 
-files="--irf=run${irf}.pt3.ch1.txt --irf=run${irf}.pt3.ch2.txt"
-for c in $corrs; do
-        files="$files run${c}.pt3.ch1.txt run${c}.pt3.ch2.txt"
-done
 
-python ~/lori/analysis/photon-tools/anisotropy $files -p2 -c2 -o fits.png >| fits.txt
+function run() {
+    files="--irf=run${irf}.pt3.ch1.txt --irf=run${irf}.pt3.ch2.txt"
+    for c in $corrs; do
+            files="$files run${c}.pt3.ch1.txt run${c}.pt3.ch2.txt"
+    done
+    args="-p2 -c2 -g1.14"
+    python ~/lori/analysis/photon-tools/anisotropy $args $files -o $out.png -J $out.json >| $out.txt
+}
+
+corrs="$cit_corrs" out=fit-cit run
+corrs="$phos_corrs" out=fit-phos run
+corrs="$cit_corrs $phos_corrs" out=fit run
