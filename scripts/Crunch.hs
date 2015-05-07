e1db43d2f7a03f59e5f378da819a6ae169d92d3a
Ben Gamari <bgamari.foss@gmail.com>
2015-05-07 14:55:56 -0400

    Rework crunch

--- scripts/Crunch.hs
+++ scripts/Crunch.hs
@@ -1,10 +1,10 @@
+import Control.Applicative
 import Development.Shake
 import Development.Shake.FilePath
 import Data.List.Split (splitOn)
-import System.FilePath.Glob
 
 dates :: [String]
-dates = words "2015-05-05 2015-05-02 2015-04-22 2015-04-20 2015-04-19 2015-04-15 2015-03-19 2015-03-12 2015-02-19 2015-02-17 2015-02-12"
+dates = words "2015-05-05 2015-05-02 2015-04-22 2015-04-20 2015-04-19 2015-04-15 2015-03-19 2015-03-12 2015-02-19 2015-02-12"
 
 data Date a = Date {year, month, day :: a}
 
@@ -13,26 +13,24 @@ parseDate s =
     let year:month:day:_ = splitOn "-" s
     in Date year month day
 
-getFiles :: Date String -> IO [FilePath]
-getFiles (Date year month day) =
-    glob $ concat ["by-date/",year,"/",year,"-",month,"/",year,"-",month,"-",day,"/*.timetag"]
+getFiles :: String -> Date String -> Action [FilePath]
+getFiles ext (Date year month day) =
+    getDirectoryFiles "." [concat ["by-date/",year,"/",year,"-",month,"/",year,"-",month,"-",day,"/*.",ext]]
 
 main :: IO ()
-main = do
-    files <- liftIO $ fmap concat $ mapM (getFiles . parseDate) dates
-    print $ length files
-    shakeArgs (shakeOptions {shakeThreads=0}) $ rules files
-
-rules files = do
-    want ["output.pdf"]
+main = shakeArgs (shakeOptions {shakeThreads=0}) $ do
+    "crunch-all" ~> do
+        files <- concat <$> mapM (getFiles "timetag" . parseDate) dates
+        liftIO $ print $ length files
+        need $ map (`addExtension` "summary.svg") files
 
     "//*.timetag.summary.svg" %> \out -> do
         let timetag = dropExtension $ dropExtension out
         need [timetag]
         need ["scripts/summarize-fcs"]
-        cmd "scripts/summarize-fcs" timetag
+        Exit c <- cmd "scripts/summarize-fcs" timetag
+        return ()
 
-    "*.pdf" %> \out -> do
-        let summaries = map (`addExtension` "summary.svg") files
-        need summaries
+    "output.pdf" %> \out -> do
+        summaries <- concat <$> mapM (getFiles "timetag.summary.svg" . parseDate) dates
         cmd "svg-nup --no-tidy --nup 1x1 --no-landscape -o" out "--" summaries
