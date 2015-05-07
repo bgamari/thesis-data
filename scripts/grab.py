c631649c4308440e440b729a2221e798317a26f1
Ben Gamari <bgamari.foss@gmail.com>
2015-05-07 13:57:25 -0400

    git-annex automatic sync

--- scripts/grab.py
+++ scripts/grab.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python
 
+import os.path
 import subprocess
-import threading
+import multiprocessing
+from glob import glob
 
 dates = '2015-05-05 2015-05-02 2015-04-22 2015-04-20 2015-04-19 2015-04-15 2015-03-19 2015-03-12 2015-02-19 2015-02-17 2015-02-12'.split()
 
 def date_path(date):
     year, month, day = date.split('-')
-    return 'data/by-date/%s/%s-%s/%s-%s-%s' % (year, year, month, year, month, day)
+    return os.path.expanduser('~/data/by-date/%s/%s-%s/%s-%s-%s' % (year, year, month, year, month, day))
 
+files = []
+for d in dates:
+    files.extend(glob(date_path(d)+'/*.timetag'))
 
-files = subprocess.check_output(['ls'] + [date_path(d)+'/*.timetag' for d in dates))
-
-pool = threading.ThreadPool()
+pool = multiprocessing.Pool(16)
 def do_file(f):
     subprocess.check_call(['summarize-fcs', f])
 
