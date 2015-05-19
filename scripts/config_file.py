50167e2215377b54f8d8881111f4d138630d1e8c
Ben Gamari <ben@smart-cactus.org>
2015-05-19 15:59:39 -0400

    find-contam: Abstract out config_file

---
+++ scripts/config_file.py
@@ -0,0 +1,14 @@
+import os.path
+import json
+
+class Config(object):
+    def __init__(self, fname='config.json'):
+        self.fname = fname
+
+    def __enter__(self):
+        self.config = json.load(open(self.fname)) if os.path.isfile(self.fname) else {}
+        return self.config
+
+    def __exit__(self, exc_type, exc, exc_tb):
+        json.dump(self.config, open(self.fname, 'w'))
+
--- scripts/find-contam.py
+++ scripts/find-contam.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python
 
-import os
-import json
 import numpy as np
 from photon_tools.bin_photons import bin_photons
 from photon_tools.io import read_photons
-
 import matplotlib.pyplot as pl
+from config_file import Config
 
 def find_runs(xs):
         runs = np.nonzero(np.diff(xs))[0] + 1
@@ -37,11 +35,13 @@ for fname in args.file:
     bursts = np.array(find_runs(counts > thresh))
     starts = t[bursts[:,0]] - args.buffer / f.jiffy
     ends = t[bursts[:,1]] + args.buffer / f.jiffy
+    print '%s: Found %d bursts above threshold of %f / bin' % (fname, len(bursts), thresh)
+
+    pl.clf()
     pl.plot(t, counts, '+')
     for start,end in zip(starts, ends):
         pl.axvspan(start, end, alpha=0.3)
     pl.savefig(fname+'-bursts.png')
 
-    config = json.load(open('config.json')) if os.path.isfile('config.json') else {}
-    config.setdefault(fname, {})['exclude'] = zip(starts * f.jiffy, ends * f.jiffy)
-    json.dump(config, open('config.json', 'w'))
+    with Config() as config:
+            config.setdefault(fname, {})['exclude'] = zip(starts * f.jiffy, ends * f.jiffy)
