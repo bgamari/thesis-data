d32e6284911a8ae8500191cd5a999c28c71f5e0c
Ben Gamari <ben@smart-cactus.org>
2015-05-26 21:07:25 +0200

    config_file: Sort_keys=True

--- scripts/config_file.py
+++ scripts/config_file.py
@@ -10,5 +10,5 @@ class Config(object):
         return self.config
 
     def __exit__(self, exc_type, exc, exc_tb):
-        json.dump(self.config, open(self.fname, 'w'), indent=4)
+        json.dump(self.config, open(self.fname, 'w'), indent=4, sort_keys=True)
 
