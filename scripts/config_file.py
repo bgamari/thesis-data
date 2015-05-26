9b59c3e75a8860b90fe7df3a698cb84e8661c5be
Ben Gamari <ben@smart-cactus.org>
2015-05-26 22:05:58 +0200

    config_file: Only rewrite if something has changed

--- scripts/config_file.py
+++ scripts/config_file.py
@@ -1,4 +1,5 @@
 import os.path
+import copy
 import json
 
 class Config(object):
@@ -7,8 +8,9 @@ class Config(object):
 
     def __enter__(self):
         self.config = json.load(open(self.fname)) if os.path.isfile(self.fname) else {}
+        self.initial_config = copy.deepcopy(self.config)
         return self.config
 
     def __exit__(self, exc_type, exc, exc_tb):
-        json.dump(self.config, open(self.fname, 'w'), indent=4, sort_keys=True)
-
+        if self.initial_config != self.config:
+            json.dump(self.config, open(self.fname, 'w'), indent=4, sort_keys=True)
